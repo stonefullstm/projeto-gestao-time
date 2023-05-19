ed2143859dc880fd06c148a2acdac662bf982f8d
stonefullstm <stonefull.stm@gmail.com>
2023-05-19 17:25:16 -0300

    feat: Implementação da classe Documento

--- src/main/java/com/trybe/gestaotime/model/Documento.java
+++ src/main/java/com/trybe/gestaotime/model/Documento.java
@@ -1,16 +1,82 @@
 package com.trybe.gestaotime.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * Classe Documento.
  */
-
+@Entity
+@Table(name = "DOCUMENTOS")
 public class Documento {
-  /**
-   * Atributos.
-   */
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String cpf;
+  @Column(name = "CARTEIRA_TRABALHO")
+  private String carteiraTrabalho;
+  @Column(name = "NUMERO_CBF")
+  private String numeroCbf;
 
   /**
-   * Métodos.
+   * Método getId.
    */
+  public Long getId() {
+    return id;
+  }
+
+  /**
+   * Método setId.
+   */
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  /**
+   * Método getCpf.
+   */
+  public String getCpf() {
+    return cpf;
+  }
+
+  /**
+   * Método setCpf.
+   */
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
+
+  /**
+   * Método getCarteiraTrabalho.
+   */
+  public String getCarteiraTrabalho() {
+    return carteiraTrabalho;
+  }
+
+  /**
+   * Método setCarteiraTrabalho.
+   */
+  public void setCarteiraTrabalho(String carteiraTrabalho) {
+    this.carteiraTrabalho = carteiraTrabalho;
+  }
+
+  /**
+   * Método setNumeroCbf.
+   */
+  public String getNumeroCbf() {
+    return numeroCbf;
+  }
+
+  /**
+   * Método getNumeroCbf.
+   */
+  public void setNumeroCbf(String numeroCbf) {
+    this.numeroCbf = numeroCbf;
+  }
+
 
 }
