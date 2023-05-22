4011a3c11ce43cf0c14161b2d662b4f67c429cdc
stonefullstm <stonefull.stm@gmail.com>
2023-05-22 09:18:38 -0300

    feat: Implementação da classe GenericDAO

--- src/main/java/com/trybe/gestaotime/dao/GenericDao.java
+++ src/main/java/com/trybe/gestaotime/dao/GenericDao.java
@@ -1,6 +1,9 @@
 package com.trybe.gestaotime.dao;
 
 import java.io.Serializable;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
 
 /**
  * Classe abstrata GenericDao.
@@ -8,12 +11,32 @@ import java.io.Serializable;
 
 public abstract class GenericDao<T, I extends Serializable> {
 
-  /**
-   * Atributos.
-   **/
+  EntityManager em =
+      Persistence.createEntityManagerFactory("crudHibernatePU").createEntityManager();
 
   /**
-   * Métodos.
-   **/
+   * Método salvar.
+   */
+  public void salvar(T entity) {
+    em.getTransaction().begin();
+    em.persist(entity);
+    em.getTransaction().commit();
+    em.close();
+  }
+
+  /**
+   * Método editar.
+   */
+  public void editar(T entity) {
+    em.getTransaction().begin();
+    em.merge(entity);
+    em.getTransaction().commit();
+  }
+
+  public abstract void deletar(Long id);
+
+  public abstract List<T> listar();
+
+  public abstract T buscarPorId(Long id);
 
 }
